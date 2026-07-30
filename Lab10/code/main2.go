09414ce4d94727965ea6c192ed810f33cd2609fe
TelmanoNeto <telmano.leite.japiassu.neto@ccc.ufcg.edu.br>
2026-07-30 16:22:02 -0300

    lab atual

---
+++ Lab10/code/main2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func mensagem() {
+	fmt.Println("Executando mensagem")
+
+}
+
+func main() {
+	go mensagem()
+	time.Sleep(time.Second)
+	fmt.Println("Fim da main")
+}
